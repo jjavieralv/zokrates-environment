5ffc286a3121bb141d52d3b32544a9da4a27d080
jjavieralv <jjavieralvarezrodriguez@gmail.com>
2024-10-08 11:08:27 +0200

    update script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zokrates-environment
 Provide an easy to use zokrates environment dockerized
+
+Script to be able to make tests easy with Zokrates using docker
+It will mount a folder into the docker container and execute there everything you need
--- scripts/zokrates_management.sh
+++ scripts/zokrates_management.sh
@@ -21,6 +21,10 @@ EXPORT_VERIFIER=0
 # or verify natively
 VERIFY=0
 
+INSPECT=0
+
+VERBOSE=""
+
 ## SUPPORT FUNCTIONS 
 function check_errors(){
     if [[ $? -ne 0 ]];then
@@ -42,46 +46,52 @@ function check_dependencies(){
 
 function compile() {
     echo "Compiling using ZoKrates with the provided code file path: ${FOLDER_TO_MOUNT}/${CODEFILE_PATH}"
-    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates compile -i ${CODEFILE_PATH}"
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates compile -i ${CODEFILE_PATH} ${VERBOSE}"
     eval $DOCKER_COMMAND
     check_errors "Compilation failed"
 }
 
 function setup() {
     echo "Setting up using ZoKrates with the provided proving scheme: ${PROVING_SCHEME}"
-    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates setup -s "
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates setup -s ${VERBOSE}"
     eval $DOCKER_COMMAND
     check_errors "Setup failed"
 }
 
 function compute_witness() {
     echo "Computing witness using ZoKrates with the provided arguments: ${WITNESS_ARGUMENTS}"
-    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates compute-witness -a ${WITNESS_ARGUMENTS}"
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates compute-witness -a ${WITNESS_ARGUMENTS} ${VERBOSE}"
     eval $DOCKER_COMMAND
     check_errors "Computing witness failed"
 }
 
 function generate_proof() {
     echo "Generating proof using ZoKrates"
-    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates generate-proof"
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates generate-proof ${VERBOSE}"
     eval $DOCKER_COMMAND
     check_errors "Proof generation failed"
 }
 
 function export_verifier() {
     echo "Exporting verifier using ZoKrates"
-    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates export-verifier"
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates export-verifier ${VERBOSE}"
     eval $DOCKER_COMMAND
     check_errors "Verifier export failed"
 }
 
 function verify() {
     echo "Verifying proof using ZoKrates"
-    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates verify"
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates verify ${VERBOSE}"
     eval $DOCKER_COMMAND
     check_errors "Verification failed"
 }
 
+function inspect() {
+    echo "Verifying proof using ZoKrates"
+    DOCKER_COMMAND="${DOCKER_BASECOMMAND} -v $(pwd)/${FOLDER_TO_MOUNT}:/tmp/zokrates_test -w /tmp/zokrates_test ${DOCKER_IMAGE} zokrates inspect -i ${INSPECT_FILE} ${VERBOSE}"
+    eval $DOCKER_COMMAND
+    check_errors "Verification failed"
+}
 
 
 
@@ -92,32 +102,33 @@ function zokrates_actions(){
     [[ $GENERATE_PROOF -eq 1 ]] && generate_proof
     [[ $EXPORT_VERIFIER -eq 1 ]] && export_verifier
     [[ $VERIFY -eq 1 ]] && verify
+    [[ $INSPECT -eq 1 ]] && inspect
 }
 
 function help_menu(){
 echo "Usage: $0 [options]"
 echo "Options:"
-echo "  -I <image>             Specify the Docker image to use (default: zokrates/zokrates:latest)."
-echo "  -c <codefile>          Compile the ZoKrates program. Specify codefile path."
-echo "  -s                     Setup phase of the ZoKrates program."
-echo "  -w                     Compute the witness for the ZoKrates program."
-echo "  -a <arguments>         Specify arguments for compute the witness."
-echo "  -p                     Generate the proof for the ZoKrates program."
+echo "  -c <codefile>          Compile the ZoKrates program. Specify codefile path(relative to -f)."
 echo "  -e                     Export the verifier smart contract."
-echo "  -v                     Verify the proof."
-echo "  -f <folder_to_mount>   Specify the folder to mount into the Docker container (default: current directory)."
 echo "  -h                     Display this help menu."
+echo "  -a <arguments>         Specify arguments for compute the witness."
+echo "  -i <file>              Inspect output file "
+echo "  -I <image>             Specify the Docker image to use (default: zokrates/zokrates:latest)."
+echo "  -p                     Generate the proof for the ZoKrates program."
+echo "  -s                     Setup phase of the ZoKrates program."
+echo "  -v                     Verify the proof."
+echo "  -V                     enable verbose mode"
+echo "  -w                     Compute the witness for the ZoKrates program."
+echo "      -a <arguments>         Specify arguments for compute the witness."
 echo ""
 echo "Example:"
 echo "  $0 -I zokrates/zokrates:0.7.6 -c mycode.zok -s -w 1 2 3 -p -e -v -f /my/folder/with/everything"
-
+exit 1
 }
 
 function main(){
     check_dependencies
     zokrates_actions
-    
-    
 }
 
 
@@ -126,9 +137,12 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
-while getopts "I:c:swpeva:f:" opt; do
+while getopts "I:i:Vc:swpeva:f:" opt; do
     case $opt in
-        i) IMAGE="$OPTARG"
+        I) IMAGE="$OPTARG"
+        ;;
+        i) INSPECT=1
+            INSPECT_FILE="$OPTARG"
         ;;
         c) COMPILE=1
             CODEFILE_PATH="$OPTARG"
@@ -149,15 +163,15 @@ while getopts "I:c:swpeva:f:" opt; do
         ;;
         v) VERIFY=1
         ;;
+        V) VERBOSE="--verbose"
+        ;;
         f) FOLDER_TO_MOUNT="$OPTARG"
         ;;
         \?) echo "Invalid option -$OPTARG" >&2
             help_menu
-            exit 1
         ;;
         :) echo "Option -$OPTARG requires an argument." >&2
             help_menu
-            exit 1
         ;;
     esac
 done
